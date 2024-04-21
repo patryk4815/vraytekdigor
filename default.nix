d071cb0e8f0c8e320e874cbfd228fcc71f3a5c8c
psondej <patryk.sondej@grupawp.pl>
2024-04-21 22:15:46 +0200

    fix flake

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ let
   # packages overlaid.
   nixpkgs = import nixpkgs_src {
     system = system;
-
+    config.allowUnsupportedSystem = true;
     crossSystem = {
       libc = "musl";
       config = "mips-unknown-linux-musl";
